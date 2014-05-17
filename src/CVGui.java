c7ae6a9d92ec9ae6788bd2aa48754f00bf1fd7a6
Alex <alex_lind2003@yahoo.de>
2014-05-17 13:17:25 +0200

    Aufgabe 1 vollständig, mit Kommentaren

--- src/CVGui.java
+++ src/CVGui.java
@@ -3,31 +3,41 @@ import java.awt.BorderLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
 
 /**
  * 
  */
 public class CVGui extends JFrame {
 	
+	//create new CurriculumVitae object
 	CurriculumVitae cv = new CurriculumVitae();
 	
 	public CVGui(){
-		super("Curriculum Vitae");
+		
+		super("Curriculum Vitae"); //set name for GUI-Window
+		
+		this.setSize(640, 480); // set frame size
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		this.setSize(640, 480);
-		JPanel mainPanel = new JPanel();
-		mainPanel.setLayout(new BorderLayout());
-		this.setContentPane(mainPanel);
+		JPanel mainPanel = new JPanel();  // create new JPanel
+		mainPanel.setLayout(new BorderLayout());  // mainPanel uses Borderlayout
+		this.setContentPane(mainPanel);  //mainPanel set as ContentPane
 		
-		JButton load =new JButton("load");
-		JButton save =new JButton("save");
-		JButton create =new JButton("create");
+		//create buttons
+		JButton load = new JButton("load");
+		JButton save = new JButton("save");
+		JButton create = new JButton("create");
 		
+		//new Panel with 3 buttons
 		JPanel lsc = new JPanel();
 		lsc.add(load);
 		lsc.add(save);
 		lsc.add(create);
-		mainPanel.add(lsc, BorderLayout.SOUTH);
+		mainPanel.add(lsc, BorderLayout.SOUTH); // Panel "lsc" at the bottom of mainPanel
+		
+		//new Panel for tabs
+		JTabbedPane jtp = new JTabbedPane();
+		mainPanel.add(jtp, BorderLayout.CENTER); //locate "jtp" in center of mainPanel
 	}
 	
 
