dd662548f9f9f9fabc908d587c1f7660fd103506
Lars Porth <lporth@students.uni-mainz.de>
2014-06-11 22:43:18 +0200

    Aufgabe 2

---
+++ src/Matrix.java
@@ -0,0 +1,32 @@
+
+public class Matrix<T extends Number> {
+	public Matrix(T[][] matrix) {
+		this.matrix = matrix;
+	}
+
+	public T[][] matrix;
+	
+	public void printMatrix(){
+		for(int i = 0; i<this.matrix.length;i++){
+			for(int j = 0; j < this.matrix.length; j++){
+				System.out.print(matrix[i][j]+" ");
+			}
+		System.out.println();	
+		}
+	}
+	public double[] vectorMult(T[] vector){
+		double[] newVector = new double[this.matrix.length];
+		for(int i = 0; i < vector.length;i++){
+			newVector[i]=0;
+		}
+		if(vector.length == this.matrix.length){
+			for(int i = 0; i < vector.length; i++){
+				for(int j = 0 ; j<vector.length;j++){
+					newVector[i]=newVector[i]+(Double.parseDouble(vector[j].toString())*Double.parseDouble(matrix[i][j].toString()));
+				}
+				//System.out.println(newVector[i]);
+			}
+		}		return newVector;
+	}
+	
+}
