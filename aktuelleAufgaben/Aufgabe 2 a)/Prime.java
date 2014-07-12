7dd7e0701630b9cd85400a467d3399b65a40728b
Alexander Lind <alind@students.uni-mainz.de>
2014-07-12 13:13:03 +0200

    Aufgabe 2 a) bereit für Abgabe an den Dateien in src kann nun für die 2 b) gearbeitet werden

---
+++ aktuelleAufgaben/Aufgabe 2 a)/Prime.java
@@ -0,0 +1,26 @@
+
+public class Prime {
+	
+	public void calc(long x){
+		 long limit = x/2;
+		 long counter =2;
+		 boolean result = false;
+		 boolean test = false;
+		
+		long begin = System.nanoTime();
+		while(counter < limit && test == false){
+			if((x % counter) == 0){
+				 result = false;
+				 test = true;				 				 				 
+			}
+			else{
+				 result = true;
+			}
+			counter ++;
+		}
+		long end = System.nanoTime();
+		System.out.println(result);
+		System.out.println("Es wurden " + (end-begin) + " Nanosekunden zur Berechnung benötigt");
+	}
+
+}
