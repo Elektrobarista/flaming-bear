15a477e52e8a475cb10d72fb092cd547d7be8110
Lars <lporth@students.uni-mainz.de>
2014-07-15 14:53:58 +0200

    Alle Threads sollten vor Rückgabe beenden.

--- src/demoThread/Prime.java
+++ src/demoThread/Prime.java
@@ -1,39 +1,72 @@
 package demoThread;
 
+import java.util.ArrayList;
+
 public class Prime implements Runnable {
 	
 	long x;
+	boolean isPrime = true;
+	Thread[] threads;
+	long lower;
+	int i;
 	
 	public Prime(long x){
 		this.x = x;
 	}
 	
-	public void calc() {
-		 long limit = x/2;
-		 long counter =2;
-		 boolean result = false;
-		 boolean test = false;
+	public boolean isPrime(long x, int i){
+		this.lower = 2;
+		this.threads = new Thread[i];
+		for (int j = 0; j < i; j++){//(x/i/2-1)%i; j++){
+			this.threads[j] = new Thread(this);
+			this.threads[j].start();
+		}
+		this.i = 0;
 		
-		long begin = System.nanoTime();
-		while(counter < limit && test == false){
-			if((x % counter) == 0){
-				 result = false;
-				 test = true;				 				 				 
+		//Starten des ersten threads dannach die restlichen threads vom ersten thread straten...
+		//////////////////////////////////////////////////////////////////////////////
+		
+		// Schleife die dafür sorgt das es keine Rückgabe gibt solange noch Threads laufen.
+		
+		boolean runningThreads = true;
+		while(isPrime && runningThreads){
+			runningThreads = false;
+			for(int k = 0; k < this.threads.length; k ++){
+				if (this.threads[k].isAlive() ==  true){
+					runningThreads = true;
+					break;
+				}
 			}
-			else{
-				 result = true;
+		}
+			
+			
+		return this.isPrime;
+	}
+	
+	
+	public void calc(long lower, long upper) {
+		 long limit = upper;
+		 long counter = lower;	
+		 
+	   	while(counter < limit && isPrime == true){
+			if((x % counter) == 0){
+				 isPrime = false;			 				 				 
 			}
 			counter ++;
 		}
-		long end = System.nanoTime();
-		System.out.println(result);
-		System.out.println("Es wurden " + (end-begin) + " Nanosekunden zur Berechnung benötigt");
 	}
 
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-		calc();
+		long threadCount = this.lower;
+		lower++;
+		System.out.println(threadCount+"Thread");
+		for (int k = 0; k < 30; k++){
+			System.out.println("Thread"+threadCount+": "+k);
+		}
+		
+		//calc();
 	}
 
 }
--- src/demoThread/Test.java
+++ src/demoThread/Test.java
@@ -1,11 +1,13 @@
 package demoThread;
 
+import java.util.ArrayList;
+
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Thread t1 = new Thread(new Prime(1000000007));
+		/*Thread t1 = new Thread(new Prime(1000000007));
 		Thread t2 = new Thread(new Prime(100000007));
 		Thread t3 = new Thread(new Prime(1000000059));
 		Thread t4 = new Thread(new Prime(10000000019L));
@@ -15,10 +17,14 @@ public class Test {
 		t3.start();
 		t4.start();
 
-		//Prime test = new Prime();
+		Prime test = new Prime();
 		
 		//test.calc(10000000019L);
-		
+		*/
+		//ArrayList<Thread> t = null;
+		//t.add(new Thread());
+		Prime test = new Prime(100);
+		System.out.println(test.isPrime(10,4));
 		
 	}
 
