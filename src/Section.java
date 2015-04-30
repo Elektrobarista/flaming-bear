22f3bc6f5fff94e2e9ff3c0f0a8bfa4da0e160fb
Alexander Lind <alind@students.uni-mainz.de>
2015-04-30 13:10:30 +0200

    Javadoc der Methoden und Klasse hinzugefügt, so wie es die CodingConvention seit neustem in Eis verlangt *kotz*
    Signed-off-by: Alexander Lind <alind@students.uni-mainz.de>

--- src/Section.java
+++ src/Section.java
@@ -1,3 +1,6 @@
+/**
+ * The <I>section</I> Class represents the content of a section in Latex
+ */
 public class Section {
 	private StringBuilder content = new StringBuilder("");
 	public Section(String name){
@@ -8,10 +11,16 @@ public class Section {
 		return content;
 	}
 	
-	public void setContent(StringBuilder content) {							//Keine Ahnung, ob man das braucht. habe mal gelernt, dass man Getter und Setter 
+	public void setContent(StringBuilder content) {							//!!Keine Ahnung, ob man das braucht. habe mal gelernt, dass man Getter und Setter 
 		this.content = content;                                            //automatisch anlegen soll, sobald die Variable "privat ist. Der Getter wird mit der Methode
-	}                                                                     //toString aber sowieso mehr oder weniger ersetzt und wie sinnvoll hier ein Setter ist, ist auch fraglich
+	}                                                                     //toString aber sowieso mehr oder weniger ersetzt und wie sinnvoll hier ein Setter ist, ist auch fraglich!!
 	
+	/**
+	 * Adds a CVLine to the section
+	 * @param val1 First String
+	 * @param val2 Second String
+	 * @throws IllegalArgumentException If the second String is empty
+	 */
 	public void addCVLine(String val1, String val2)throws IllegalArgumentException{
 			if (!val2.equals("")){
 				content.append("\n\\cvline{" + val1 + "}" + "{" + val2 + "}");
@@ -21,6 +30,11 @@ public class Section {
 			}
 		
 	}
+	/**
+	 * Adds a CVEntry to the section
+	 * @param vals Can take multiple Strings. The first an second String must not be empty.There can not be more then 6 Strings in one CVEntry
+	 * @throws IllegalArgumentException If first and second String is empty and/or more then 6 Strings 
+	 */
 	public void addCVEntry(String... vals)throws IllegalArgumentException{
 		if (vals.length <= 6 && (!vals[0].equals("") && !vals[1].equals(""))){ //Überprüfung, ob weniger als 6 Argumente vorhanden sind und die ersten beiden NICHT leer sind
 			content.append("\n\\cventry");
@@ -32,6 +46,10 @@ public class Section {
 			throw new IllegalArgumentException("Erste zwei Argumente sind leer, oder mehr als 6 Argumente");
 		}		
 	}
+	/**
+	 * Turns the internal <I>StringBuilder-variable</I> into a String for normal ouput
+	 * @return Content of the section as String
+	 */
 	public String toString(){
 		return content.toString();
 	}
