ce42f64a6e152e563276bbf631b22beb7485fd2d
Nina Metzinger <nmetzing@students.uni-mainz.de>
2015-05-09 19:54:04 +0200

    läuft jetzt ^^

--- src/CaesarWriter.java
+++ src/CaesarWriter.java
@@ -17,20 +17,23 @@ public class CaesarWriter extends java.io.Writer{
 	
 	@Override
 	public void write(char[] cbuf, int off, int len) throws IOException {
-		// TODO Auto-generated method stub
-		
+		if (cbuf==null || off >=cbuf.length || off+len>cbuf.length){
+			throw new IOException("IO-Error");
+		}
+		char[] cipher = new char[len];
+		for (int i=0; i<len; i++){
+			cipher[i] = caesar.encode(cbuf[i+off]);
+		}
+		base.write(cipher);
 	}
 
 	@Override
 	public void flush() throws IOException {
-		// TODO Auto-generated method stub
-		
+		base.flush();
 	}
 
 	@Override
 	public void close() throws IOException {
-		
-		// TODO Auto-generated method stub
-		
+		base.close();
 	}	
 }
