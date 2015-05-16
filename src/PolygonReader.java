7f38584aa2450b80b84216a5039abc7d96af8126
Nina Metzinger <nmetzing@students.uni-mainz.de>
2015-05-16 23:46:14 +0200

    ..

---
+++ src/PolygonReader.java
@@ -0,0 +1,21 @@
+import java.awt.Point;
+
+
+public class PolygonReader {
+	
+	String polyFile;
+	
+	public PolygonReader(String polyFile){
+		this.polyFile = polyFile;		
+	}
+	
+	public boolean next(){
+		return true;
+	}
+	
+	public Point get(){
+		
+		return null;
+	}
+
+}
---
+++ src/PolygonViewer.java
@@ -0,0 +1,12 @@
+
+public class PolygonViewer {
+	
+	public PolygonViewer(String polyFile){
+		
+	}
+	
+	public void paint(Graphics g){
+		super.paint(g);
+	}
+
+}
