03ad00592d5dde0920384a3a0494303e793a8e71
Nina Metzinger <nmetzing@students.uni-mainz.de>
2015-06-20 15:13:16 +0200

    Aufgabe 1 ohne javadocs

---
+++ src/ComparingLists.java
@@ -0,0 +1,109 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+
+public class ComparingLists {
+
+	public static void main(String[] args) {
+		long start;
+		long stop;
+		long diff;
+		ArrayList<Integer> arr = new ArrayList<Integer>();
+		LinkedList<Integer> lin = new LinkedList<Integer>();
+		
+		// stopping time for adding Elements at the end of the two list types
+		System.out.println("a)");
+		start = System.currentTimeMillis();
+		for (int i=0; i<100000; i++){
+			arr.add(i);
+		}
+		stop = System.currentTimeMillis();
+		diff = stop - start;
+		System.out.println("Zeit fuer das Hinzufuegen von 100000 Elementen an das Ende einer Array List: "+ diff);
+		
+		start = System.currentTimeMillis();
+		for (int i=0; i<100000; i++){
+			lin.addLast(i);
+		}
+		stop = System.currentTimeMillis();
+		diff = stop - start;
+		System.out.println("Zeit fuer das Hinzufuegen von 100000 Elementen an das Ende einer Linked List: "+ diff);
+		
+		// stopping time for removing Elements at the end of the two list types 
+		start = System.currentTimeMillis();
+		for (int i=99999; i>=0; i--){
+			arr.remove(i);
+		}
+		stop = System.currentTimeMillis();
+		diff = stop - start;
+		System.out.println("Zeit fuer das Entfernen von 100000 Elementen an das Ende einer Array List: "+ diff);
+		
+		start = System.currentTimeMillis();
+		for (int i=0; i<100000; i++){
+			lin.removeLast();
+		}
+		stop = System.currentTimeMillis();
+		diff = stop - start;
+		System.out.println("Zeit fuer das Entfernen von 100000 Elementen an das Ende einer Linked List: "+ diff);
+		
+		// stopping time for adding Elements at the head of the two list types
+		System.out.println("b)");
+		start = System.currentTimeMillis();
+		for (int i=0; i<100000; i++){
+			arr.add(0,i);
+		}
+		stop = System.currentTimeMillis();
+		diff = stop - start;
+		System.out.println("Zeit fuer das Hinzufuegen von 100000 Elementen an den Anfang einer Array List: "+ diff);
+		
+		start = System.currentTimeMillis();
+		for (int i=0; i<100000; i++){
+			lin.addFirst(i);
+		}
+		stop = System.currentTimeMillis();
+		diff = stop - start;
+		System.out.println("Zeit fuer das Hinzufuegen von 100000 Elementen an den Anfang einer Linked List: "+ diff);
+		
+		// stopping time for removing Elements at the head of the two list types 
+		start = System.currentTimeMillis();
+		for (int i=0; i<100000; i++){
+			arr.remove(0);
+		}
+		stop = System.currentTimeMillis();
+		diff = stop - start;
+		System.out.println("Zeit fuer das Entfernen von 100000 Elementen an den Anfang einer Array List: "+ diff);
+		
+		start = System.currentTimeMillis();
+		for (int i=0; i<100000; i++){
+			lin.removeFirst();
+		}
+		stop = System.currentTimeMillis();
+		diff = stop - start;
+		System.out.println("Zeit fuer das Entfernen von 100000 Elementen an den Anfang einer Linked List: "+ diff);
+		
+		// add Elements at the end of the two list types
+		System.out.println("c)");
+		for (int i=0; i<100000; i++){
+			arr.add(i);
+		}
+		for (int i=0; i<100000; i++){
+			lin.addLast(i);
+		}
+		// Stopping time for reading an entry for 100000 elements
+		start = System.currentTimeMillis();
+		for (int i=1; i<100000; i++){
+			arr.get(i);
+		}
+		stop = System.currentTimeMillis();
+		diff = stop - start;
+		System.out.println("Zeit fuer das Auslesen von 100000 Elementen mittels get-Methode einer Array List: "+ diff);
+				
+		start = System.currentTimeMillis();
+		for (int i=0; i<100000; i++){
+			lin.get(i);
+		}
+		stop = System.currentTimeMillis();
+		diff = stop - start;
+		System.out.println("Zeit fuer das Auslesen von 100000 Elementen mittels get-Methode einer Linked List: "+ diff);
+	}
+}
